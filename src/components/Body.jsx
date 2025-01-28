ef60f854050982665979749af96b26f9fe9284f0
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-28 23:52:18 +0530

    Building Card with half functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaFileWord } from "react-icons/fa6";
 
 const Body = () => {
+  const [selectFile, setSelectFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectFile(e.target.files[0]);
+  };
   return (
     <>
       <div className="max-w-screen-2xl mx-auto container px-6 py-3 md:px-40 flex flex-col justify-center items-center h-screen">
         <div className="border-2 border-dashed border-blue-500 p-5 shadow-lg rounded-lg h-96 flex flex-col  items-center">
           <div className="text-center m-3 p-2">
-            <h1 className="text-3xl font-bold">
-              Convert Word to PDF Online
-            </h1>
+            <h1 className="text-3xl font-bold">Convert Word to PDF Online</h1>
           </div>
           <div className="text-center mb-3">
             <p className="text-sm m-3 ">
@@ -17,14 +21,25 @@ const Body = () => {
             </p>
           </div>
           <div className="flex flex-col items-center space-y-4">
-            <input type="file"
-            accept=".doc, .docx"
-            className=""
-            id="fileInput"
+            <input
+              type="file"
+              accept=".doc, .docx"
+              className=""
+              onChange={handleFileChange}
+              id="fileInput"
             />
-            <label htmlFor="fileInput"className="w-full bg-gray-100 text-gray-700 flex items-center justify-center px-4 py-6 rounded-lg shadow-lg"></label>
+            <label
+              htmlFor="fileInput"
+              className="w-full bg-gray-100 text-gray-700 flex items-center justify-center px-4 py-6 rounded-lg shadow-lg"
+            >
+              <FaFileWord className="text-3xl mr-3" />
+            </label>
           </div>
-          <button className="text-white bg-blue-500 hover:bg-blue700 disabled:bg-gray-400 disabled:pointer-events-none duration-300  text-lg px-4 py-2 rounded-lg font-bold m-3">Convert File</button>
+          <button
+            className="text-white bg-blue-500 hover:bg-blue700 disabled:bg-gray-400 disabled:pointer-events-none duration-300  text-lg px-4 py-2 rounded-lg font-bold m-3"
+          >
+            Convert File
+          </button>
         </div>
       </div>
     </>
